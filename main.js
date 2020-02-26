8ca289686c8459c3cb80c7be21b529391cc7642b
DENIS SAVGIR <denis@DENISs-MacBook-Pro.local>
2020-02-26 15:31:18 -0500

    Done.

--- main.js
+++ main.js
@@ -85,12 +85,14 @@ function trimStart(string) {
 
 function trimEnd(string) {
   let i;
-
+  
   for(i = string.length - 1; i >= 0; i--) {
     if(string[i] != ' ') {
-      return slice(string, 0, i + 1);
+      break;
     }
   }
+
+  return slice(string, 0, i + 1);
 }
 
 /*********************************

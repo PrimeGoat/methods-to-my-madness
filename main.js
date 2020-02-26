61d2b5f63ad87e38d8bfc1d33858abf477940719
DENIS SAVGIR <denis@DENISs-MBP.home>
2020-02-25 21:38:55 -0500

    Done.

--- main.js
+++ main.js
@@ -2,10 +2,101 @@
  * YOUR CODE HERE *
  ******************/
 
+function slice(string, start = 0, end = '') {
+  let out = "";
+  if(end === '') {
+    end = string.length;
+  }
+  for(let i = start; i < Math.min(string.length, end); i++) {
+    out += string[i];
+  }
 
+  return out;
+}
 
+function repeat(string, count) {
+  out = '';
+  for(let i = 0; i < count; i++) {
+    out += string;
+  }
 
+  return out;
+}
 
+function startsWith(string, substring) {
+  return substring === slice(string, 0, substring.length);
+}
+
+function endsWith(string, substring) {
+  return substring === slice(string, string.length - substring.length, string.length);
+}
+
+function includes(array, item) {
+  for(let i = 0; i < array.length; i++) {
+    if(array[i] === item) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function join(array, separator = '') {
+  let out = '';
+  for(let i = 0; i < array.length; i++) {
+    out += array[i];
+    if(i + 1 < array.length) {
+      out += separator;
+    }
+  }
+
+  return out;
+}
+
+function split(string, split = '') {
+  let out = [];
+  let current = '';
+
+  for(let i = 0; i < string.length; i++) {
+    if(string[i] == split[0]) {
+      out.push(current);
+      current = '';
+    } else {
+      current += string[i];
+    }
+  }
+  out.push(current);
+
+  return out;
+}
+
+function trimStart(string) {
+  let out = '';
+  let doneStart = 0;
+
+  for(let i = 0; i < string.length; i++) {
+    if(string[i] != ' ' || doneStart) {
+      out += string[i];
+      doneStart = 1;
+    }
+  }
+
+  return out;
+}
+
+function trimEnd(string) {
+  let out = [];
+  let doneEnd = 0;
+
+  for(let i = string.length - 1; i >= 0; i--) {
+    if(string[i] != ' ' || doneEnd) {
+      out.push(string[i]);
+      doneEnd = 1;
+    }
+  }
+  out.reverse();
+
+  return join(out);
+}
 
 /*********************************
  * OUR CODE BELOW; DO NOT TOUCH! *
